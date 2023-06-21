882156a1f8d77759a85f490d5444328392712af4
githubuiux <uiuxista@gmail.com>
2023-06-21 11:30:06 +0300

    Initial Commit

---
+++ TetrisGame/TetrisGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisGameApp.swift
+//  TetrisGame
+//
+//  Created by Levent on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TetrisGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
